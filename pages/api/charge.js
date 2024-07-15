adaf5f52a5fb84f5fb636e11bc5b9a6a026d80e4
Imamzfrr <80440606+imamzfrr@users.noreply.github.com>
2024-07-15 19:46:01 +0700

    repair charge.js

--- pages/api/charge.js
+++ pages/api/charge.js
@@ -1,10 +1,13 @@
-// pages/api/charge.js
+import fetch from 'node-fetch';
+import { Buffer } from 'buffer';
+
 export default function handler(req, res) {
-    if (req.method === 'POST') {
-      const serverKey = 'SB-Mid-server-7kRmSxtejJcnziEVnW6aW1VS'; // Ganti dengan server key Anda
-      const apiUrl = 'https://app.sandbox.midtrans.com/snap/v1/transactions'; // URL API sandbox
-  
-      const chargeAPI = async (apiUrl, serverKey, requestBody) => {
+  if (req.method === 'POST') {
+    const serverKey = 'SB-Mid-server-7kRmSxtejJcnziEVnW6aW1VS'; // Ganti dengan server key Anda
+    const apiUrl = 'https://app.sandbox.midtrans.com/snap/v1/transactions'; // URL API sandbox
+
+    const chargeAPI = async (apiUrl, serverKey, requestBody) => {
+      try {
         const response = await fetch(apiUrl, {
           method: 'POST',
           headers: {
@@ -14,23 +17,26 @@ export default function handler(req, res) {
           },
           body: requestBody
         });
-  
+
         const data = await response.json();
         return { body: data, http_code: response.status };
-      };
-  
-      const requestBody = JSON.stringify(req.body);
-  
-      chargeAPI(apiUrl, serverKey, requestBody)
-        .then(result => {
-          res.status(result.http_code).json(result.body);
-        })
-        .catch(error => {
-          res.status(500).json({ message: error.message });
-        });
-  
-    } else {
-      res.setHeader('Allow', ['POST']);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
+      } catch (error) {
+        throw new Error('Error in API call: ' + error.message);
+      }
+    };
+
+    const requestBody = JSON.stringify(req.body);
+
+    chargeAPI(apiUrl, serverKey, requestBody)
+      .then(result => {
+        res.status(result.http_code).json(result.body);
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
+}
