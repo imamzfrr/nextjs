0bd405577dc5840f3a88d4ec2b50a275a2cbf334
Imamzfrr <80440606+imamzfrr@users.noreply.github.com>
2024-07-16 00:12:29 +0700

    Update notification.js
    add memori

--- pages/api/notification.js
+++ pages/api/notification.js
@@ -1,32 +1,39 @@
+// pages/api/notification.js
+
+let transactions = {};
+
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const notification = req.body;
-    console.log('Midtrans Notification:', notification);
+    if (req.method === 'POST') {
+        const notification = req.body;
+        console.log('Midtrans Notification:', notification);
 
-    // Logika untuk memproses notifikasi
-    switch (notification.transaction_status) {
-      case 'settlement':
-        console.log('Transaksi berhasil:', notification);
-        break;
-      case 'pending':
-        console.log('Transaksi pending:', notification);
-        break;
-      case 'deny':
-        console.log('Transaksi ditolak:', notification);
-        break;
-      case 'expire':
-        console.log('Transaksi kadaluarsa:', notification);
-        break;
-      case 'cancel':
-        console.log('Transaksi dibatalkan:', notification);
-        break;
-      default:
-        console.log('Status transaksi tidak dikenal:', notification.transaction_status);
+        // Simpan notifikasi ke dalam memori
+        transactions[notification.order_id] = notification;
+
+        // Logika untuk memproses notifikasi
+        switch (notification.transaction_status) {
+            case 'settlement':
+                console.log('Transaksi berhasil:', notification);
+                break;
+            case 'pending':
+                console.log('Transaksi pending:', notification);
+                break;
+            case 'deny':
+                console.log('Transaksi ditolak:', notification);
+                break;
+            case 'expire':
+                console.log('Transaksi kadaluarsa:', notification);
+                break;
+            case 'cancel':
+                console.log('Transaksi dibatalkan:', notification);
+                break;
+            default:
+                console.log('Status transaksi tidak dikenal:', notification.transaction_status);
+        }
+
+        res.status(200).json({ message: 'Notification received and transaction status updated' });
+    } else {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
     }
-
-    res.status(200).json({ message: 'Notification received' });
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
 }
