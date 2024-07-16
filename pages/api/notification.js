61b4fcf5d305b2521b6c1f10ed397b71a6ece176
Imamzfrr <80440606+imamzfrr@users.noreply.github.com>
2024-07-16 13:24:09 +0700

    comeback notification.js

--- pages/api/notification.js
+++ pages/api/notification.js
@@ -1,32 +1,22 @@
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const notification = req.body;
-        console.log('Midtrans Notification:', notification);
-
-        // Logika untuk memproses notifikasi
-        switch (notification.transaction_status) {
-            case 'settlement':
-                console.log('Transaksi berhasil:', notification);
-                break;
-            case 'pending':
-                console.log('Transaksi pending:', notification);
-                break;
-            case 'deny':
-                console.log('Transaksi ditolak:', notification);
-                break;
-            case 'expire':
-                console.log('Transaksi kadaluarsa:', notification);
-                break;
-            case 'cancel':
-                console.log('Transaksi dibatalkan:', notification);
-                break;
-            default:
-                console.log('Status transaksi tidak dikenal:', notification.transaction_status);
-        }
-
-        res.status(200).json({ message: 'Notification received and transaction status updated' });
-    } else {
-        res.setHeader('Allow', ['POST']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
-}
+  if (req.method === 'POST') {
+    const notification = req.body;
+    console.log('Midtrans Notification:', notification);
+    // Logika untuk memproses notifikasi
+    switch (notification.transaction_status) {
+      case 'settlement':
+        console.log('Transaksi berhasil:', notification);
+        break;
+      case 'pending':
+        console.log('Transaksi pending:', notification);
+        break;
+      case 'deny':
+        console.log('Transaksi ditolak:', notification);
+        break;
+      case 'expire':
+        console.log('Transaksi kadaluarsa:', notification);
+        break;
+      case 'cancel':
+        console.log('Transaksi dibatalkan:', notification);
+        break;
+      default:
